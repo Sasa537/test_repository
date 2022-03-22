30e3aef7a75dab7b56290ad359ab3d781b4ccfc6
Sasa537 <96684410+Sasa537@users.noreply.github.com>
2022-03-22 18:34:53 +0300

    Update and rename src/programm/MainClass.java to источник/программа/MainClass.java

--- источник/программа/MainClass.java
+++ источник/программа/MainClass.java
@@ -2,6 +2,6 @@ package programm;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println("Hello SASA");
+        System.out.println("Hello ");
     }
 }
